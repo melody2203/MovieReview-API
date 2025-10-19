f4e8a769477c73de2bfd8c4a8bc701965ac248bc
Merertu Philipose <merertuphilip@gmail.com>
2025-10-19 09:44:14 +0300

    connect to the pythonanywhere.com

--- config/config/settings.py
+++ config/config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-dev-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['merertuph.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
